aac71a5553adcc5c3ec9b440dcc67367a619dacb
Arman KZ <armansauyenov@gmail.com>
2018-10-23 17:20:04 +0600

    Add project files.

---
+++ JS_Lesson2/JavaScript1.js
@@ -0,0 +1,31 @@
+﻿
+
+var obj = {
+    Name: "Arman",
+    Surname: "Sauyenov",
+    Age: "28"
+}
+
+Object.defineProperty(obj, 'Birthday', {
+    value: "25 October 1989",
+    enumberable: false
+})
+
+Object.defineProperty(obj, 'fullname', {
+    enumerable: true,
+    get: function () {
+        return obj.Name + " " + obj.Surname + ",  " + obj.Age;
+    },
+    set: function (value) {
+        this.Name = value.substring(0, value.indexof(" ")); //value.Split(' ')[].Length()+1;
+        value = value.substring(value.indexof(" "));
+        this.Surname = value.substring(0, value.indexof(" "));
+        value = value.substring(value.indexof(" "));
+        this.Age = value.substring(0, value.indexof(" "));
+    }
+}
+)
+
+for (var i in obj) {
+    document.write(i + " " + obj[i] + '<br>')
+}
